b9ce109b4ccd04ea1ae45edd36acf20bd6e07e27
Byron <byronh@gmail.com>
2014-05-19 11:23:03 -0700

    Tried to fix flickering on Mac OS X

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Press [Enter] to open a GLFW window..." << std::endl;
+	std::cin.get();
+
 	GLFWwindow* window;
 
 	if (!glfwInit())
@@ -15,13 +21,21 @@ int main(void)
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.0f, 0.5f, 0.25f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
+
+	std::cout << "Finished, press [Enter] to continue..." << std::endl;
+	std::cin.get();
+
 	return 0;
 }
