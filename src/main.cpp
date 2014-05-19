78a34dc4d2db6c562b221019e93c448c769a6df8
Byron <byronh@gmail.com>
2014-05-19 02:14:57 -0700

    Added GLFW window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-	std::cout << "Hello World!" << std::endl;
-	std::cin.get();
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
